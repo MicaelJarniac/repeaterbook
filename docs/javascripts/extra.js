256ca2a55f33741e8a1738f7c88dd32a674f6004
Micael Jarniac <micael@jarniac.dev>
2026-02-03 11:09:58 -0300

    feat: update Cruft

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,8 @@
+/**
+ * Custom JavaScript for RepeaterBook documentation
+ *
+ * Add any custom scripts here to enhance functionality.
+ */
+
+// Example: Log page load
+// console.log('RepeaterBook documentation loaded');
